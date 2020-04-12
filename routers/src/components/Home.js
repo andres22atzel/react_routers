632b8330615434e604928baa1c85835b8ecd8d2a
andres22atzel <andres.atzel@gmail.com>
2020-04-12 10:32:49 -0500

    Using Axios to fetch data from dummy rest api and the use of lifecycle hook componentDidMount() function

--- routers/src/components/Home.js
+++ routers/src/components/Home.js
@@ -1,18 +1,39 @@
-import React from "react";
+import React, { Component } from "react";
+import axios from "axios";
 
-const Home = () => {
-  return (
-    <div className="container">
-      <h4 className="center">Home</h4>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quasi vel
-        accusantium tempora labore maiores voluptatum totam architecto
-        reprehenderit eius, porro deserunt veritatis voluptates explicabo sint
-        tenetur natus rerum expedita. Quibusdam.
-      </p>
-    </div>
-  );
-};
-
-
-export default Home;
+export default class Home extends Component {
+  state = {
+    posts: [],
+  };
+  componentDidMount() {
+    axios.get("https://jsonplaceholder.typicode.com/posts").then((res) => {
+      console.log(res);
+      this.setState({
+        posts: res.data.slice(0, 10),
+      });
+    });
+  }
+  render() {
+    const { posts } = this.state;
+    const postList = posts.length ? (
+      posts.map((post) => {
+        return (
+          <div className="post card" key={post.id}>
+            <div className="card-content">
+              <span className="card-title">{post.title}</span>
+              <p>{post.body}</p>
+            </div>
+          </div>
+        );
+      })
+    ) : (
+      <div className="center">No posts yer</div>
+    );
+    return (
+      <div className="container">
+        <h4 className="center">Home</h4>
+        <div>{postList}</div>
+      </div>
+    );
+  }
+}
--- routers/src/components/Navbar.js
+++ routers/src/components/Navbar.js
@@ -4,14 +4,14 @@ import React from "react";
 //by props
 import { Link, NavLink, withRouter } from "react-router-dom";
 
-const Navbar = (props) => {
-  setTimeout(() => {
-    props.history.push("/about");
-  }, 2000);
+const Navbar = (/*props*/) => {
+  // setTimeout(() => {
+  //   props.history.push("/about");
+  // }, 2000);
   return (
     <nav className="nav-wrapper red darken-3">
       <div className="container">
-        <a className="brand-logo">Poke'Times</a>
+        <a href="# " className="brand-logo">Poke'Times</a>
         <ul className="right">
           <li>
             {/* Here we use a link tag */}
